a1e4c7acbe651de4ce321e694b4ff7b7e1b2b7e8
Alan Viars <aviars@videntity.com>
2020-12-07 14:09:51 -0500

    Add MongoDB-based provider directory

--- apps/provider_directory/urls.py
+++ apps/provider_directory/urls.py
@@ -1,18 +1,35 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import (practitioner_fhir_search, organization_fhir_search,
-                    practitioner_fhir_read, organization_fhir_read)
+from .views import (fhir_read,
+                    practitioner_fhir_search,
+                    practitionerrole_fhir_search,
+                    organization_fhir_search,
+                    organizationaffiliation_fhir_search,
+                    location_fhir_search, endpoint_fhir_search,
+                    insuranceplan_fhir_search, healthcareservice_fhir_search,
+                    )
 
 admin.autodiscover()
 
 # Copyright Videntity Systems, Inc. 2020
 
 fhir4 = [
-     url(r'Practitioner/(?P<id>[^/]+)', practitioner_fhir_read, name="practitioner_fhir_read"),
-     url(r'Organization/(?P<id>[^/]+)', organization_fhir_read, name="organization_fhir_read"),
-     url(r'Practitioner', practitioner_fhir_search, name="practitioner_fhir_search"),
-     url(r'Organization', organization_fhir_search, name="organization_fhir_search"),
-    
+    # Read
+    url(r'(?P<fhir_resource_type>[^/]+)/(?P<id>[^/]+)', fhir_read, name="fhir_read"),
+
+    # Search
+    url(r'PractitionerRole', practitionerrole_fhir_search,
+        name="practitionerrole_fhir_search"),
+    url(r'Practitioner', practitioner_fhir_search, name="practitioner_fhir_search"),
+
+    url(r'Organization', organization_fhir_search, name="organization_fhir_search"),
+    url(r'OrganizationAffiliation', organizationaffiliation_fhir_search,
+        name="organizationaffiliation_fhir_search"),
+    url(r'Location', location_fhir_search, name="location_fhir_search"),
+    url(r'Endpoint', endpoint_fhir_search, name="endpoint_fhir_search"),
+    url(r'InsurancePlan', insuranceplan_fhir_search, name="insuranceplan_fhir_search"),
+    url(r'HealthcareService', healthcareservice_fhir_search,
+        name="healthcareservice_fhir_search"),
 ]
 
 urlpatterns = [
